85adb082480cec05a26b44c29572cb0ac951a9b1
raneemkhalil <raneem.alwazany@gmail.com>
2023-12-14 00:39:31 +0200

    some refactors

--- lib/models/category_item_model.dart
+++ lib/models/category_item_model.dart
@@ -17,55 +17,13 @@ List<CategoryItemModel> listOfCategorys = [
     label: 'New Arrivals',
     number: 208,
     imgUrl:
-        "https://www.seasaltcornwall.com/media/catalog/product/b/_/b_wm30508_31799_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=795&width=530&canvas=530:795",
+        "https://www.seasaltcornwall.com/media/catalog/product/b/_/b_wm34707_29637_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:",
   ),
   CategoryItemModel(
     id: '2',
-    label: 'New Arrivals',
-    number: 208,
+    label: 'cheapest',
+    number: 100,
     imgUrl:
-        "https://www.seasaltcornwall.com/media/catalog/product/b/_/b_wm30508_31799_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=795&width=530&canvas=530:795",
-  ),
-  CategoryItemModel(
-    id: '1',
-    label: 'All',
-    number: 1,
-    imgUrl:
-        'https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png',
-  ),
-  CategoryItemModel(
-    id: '2',
-    label: 'Latest',
-    number: 2,
-    imgUrl:
-        'https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png',
-  ),
-  CategoryItemModel(
-    id: '3',
-    label: 'Most Popular',
-    number: 3,
-    imgUrl:
-        'https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png',
-  ),
-  CategoryItemModel(
-    id: '4',
-    label: 'Cheapest',
-    number: 4,
-    imgUrl:
-        'https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png',
-  ),
-  CategoryItemModel(
-    id: '5',
-    label: 'Hot',
-    number: 5,
-    imgUrl:
-        'https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png',
-  ),
-  CategoryItemModel(
-    id: '6',
-    label: 'New',
-    number: 6,
-    imgUrl:
-        'https://freepngimg.com/thumb/nike/28258-8-nike-logo-transparent-background.png',
-  ),
+        "https://www.seasaltcornwall.com/media/catalog/product/b/_/b_wm30376_29654_1_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:",
+  )
 ];
